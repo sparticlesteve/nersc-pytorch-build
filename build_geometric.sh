30347a39a86acf12463f5208853af8469c51a16e
Steve Farrell <sparticlesteve@gmail.com>
2019-05-14 16:40:32 -0700

    adding installation script for pytorch geometric

---
+++ build_geometric.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This script installs PyTorch geometric and its dependencies
+# https://github.com/rusty1s/pytorch_geometric
+
+# Configure the installation
+source ./config.sh
+conda activate $INSTALL_DIR
+echo "Building PyTorch Geometric on $(hostname)"
+
+# Install the packages via pip
+pip install --verbose --no-cache-dir torch-scatter
+pip install --verbose --no-cache-dir torch-sparse
+pip install --verbose --no-cache-dir torch-cluster
+pip install --verbose torch-geometric
