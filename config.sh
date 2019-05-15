36ba31550fe9056be513a9e9db7832ebaddd7d96
Steve Farrell <sparticlesteve@gmail.com>
2019-05-15 11:03:08 -0700

    special config settings for swowner

--- config.sh
+++ config.sh
@@ -1,12 +1,18 @@
 # Source me
 
+if [ $USER == "swowner" ]; then
+    umask 002 # all-readable
+    INSTALL_BASE=/usr/common/software
+else
+    INSTALL_BASE=$SCRATCH/conda
+
 # Configure the installation
 export INSTALL_NAME="pytorch"
 export PYTORCH_VERSION="v1.1.0"
 export PYTORCH_URL=https://github.com/pytorch/pytorch
 export VISION_VERSION="v0.2.2"
 export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION
-export INSTALL_DIR=$SCRATCH/conda/$INSTALL_NAME/$PYTORCH_VERSION
+export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION
 
 # Setup programming environment
 module unload PrgEnv-intel
