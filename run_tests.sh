fe9afcc4a2b21341ac2210473a36c195c6a1ea5d
Steve Farrell <sparticlesteve@gmail.com>
2019-07-04 12:26:40 -0700

    new tests for GPUs

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+. config.sh
+conda activate $INSTALL_DIR
+module load esslurm
+
+# 1 node, 8 ranks
+srun -C gpu -N 1 --gres=gpu:8 --exclusive \
+    -n 8 -c 10 -t 5 -u -l \
+    python test_install.py
+
+# 2 nodes, 16 ranks
+srun -C gpu -N 2 --gres=gpu:8 --exclusive \
+    -n 16 -c 10 -t 5 -u -l \
+    python test_install.py
--- test_install.py
+++ test_install.py
@@ -1,11 +1,20 @@
+import torch
 import torch.distributed as dist
 
-def main():
+def test_mpi():
     dist.init_process_group(backend='mpi')
     rank = dist.get_rank()
     n_ranks = dist.get_world_size()
     print('MPI process group successfully initialized!',
           'World size: {0}, Rank: {1}'.format(n_ranks, rank))
+
+def test_cuda():
+    print('CUDA available:', torch.cuda.is_available())
+    print('CUDA device:', torch.cuda.current_device())
+
+def main():
+    test_mpi()
+    test_cuda()
 
 if __name__ == '__main__':
     main()
