b599cdfb4e6e902b10a05d22c3b4dabd277085a2
Steve Farrell <sparticlesteve@gmail.com>
2019-08-12 10:13:18 -0700

    update config for new cori gpu system config

--- config.sh
+++ config.sh
@@ -11,18 +11,17 @@ fi
 export INSTALL_NAME="pytorch"
 export PYTORCH_VERSION="v1.1.0"
 export PYTORCH_URL=https://github.com/pytorch/pytorch
-export VISION_VERSION="v0.2.2"
+export VISION_VERSION="v0.3.0"
 export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/${PYTORCH_VERSION}-gpu
 export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/${PYTORCH_VERSION}-gpu
 
 # Setup programming environment
 module load gcc/7.3.0
-module load cuda/10.0
-module load nccl/2.4.2
-module load openmpi/3.1.0-ucx
+module load cuda/10.1.168
+module load openmpi/4.0.1-ucx-1.6
 
 # Setup conda
-source /usr/common/software/python/3.6-anaconda-5.2/etc/profile.d/conda.sh
+source /usr/common/software/python/3.7-anaconda-2019.07/etc/profile.d/conda.sh
 
 # Print some stuff
 echo "Configuring on $(hostname) as $USER"
