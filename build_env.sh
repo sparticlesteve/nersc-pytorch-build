b79e6b9e9058ece1d20050f1067d62ac7421720b
Steve Farrell <sparticlesteve@gmail.com>
2019-08-12 10:09:38 -0700

    updated conda env building config in accordance with pytorch build instructions and trial and error

--- build_env.sh
+++ build_env.sh
@@ -4,9 +4,11 @@
 source ./config.sh
 
 # Make a conda env
-conda create -y --prefix $INSTALL_DIR python=3.7 \
-    numpy pyyaml setuptools cmake cffi typing \
+conda create -y --prefix $INSTALL_DIR python=3.6 \
+    mkl mkl-include numpy pyyaml setuptools cmake cffi typing \
     h5py ipython ipykernel matplotlib scikit-learn pandas
 
+# Installs from other channels
 conda activate $INSTALL_DIR
+conda install -y -c pytorch magma-cuda101
 conda install -y -c conda-forge ipympl
