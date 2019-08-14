61ebff839c8baba6c246bdd3722331943f093a4f
Steve Farrell <sparticlesteve@gmail.com>
2019-08-13 23:41:27 -0700

    Now use srun --pty to setup build node with gpus visible

--- launch_build_node.sh
+++ launch_build_node.sh
@@ -2,4 +2,5 @@
 
 module load esslurm
 #salloc -C gpu --gres=gpu:1 -c 10 -t 4:00:00
-salloc -C gpu --gres=gpu:8 --exclusive -t 2:00:00
+#salloc -C gpu --gres=gpu:8 --exclusive -t 2:00:00
+srun -C gpu -N 1 -c 80 --gres=gpu:8 --exclusive -t 2:00:00 --pty bash -l
