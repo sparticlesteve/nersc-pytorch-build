a65ebf7fbb83ba0a687c0b4ee705046a006e0c4a
Steve Farrell <sparticlesteve@gmail.com>
2019-08-14 11:41:26 -0700

    Updating to pytorch v1.2.0

--- config.sh
+++ config.sh
@@ -9,9 +9,9 @@ fi
 
 # Configure the installation
 export INSTALL_NAME="pytorch"
-export PYTORCH_VERSION="v1.1.0"
+export PYTORCH_VERSION="v1.2.0"
 export PYTORCH_URL=https://github.com/pytorch/pytorch
-export VISION_VERSION="v0.2.2"
+export VISION_VERSION="v0.3.0"
 export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION
 export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION
 
