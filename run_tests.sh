d78d5a0b1aa550acde158d16f5873b7b22352855
Steve Farrell <sparticlesteve@gmail.com>
2019-09-17 15:21:25 -0700

    move craydl test into main run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -7,4 +7,9 @@
 . config.sh
 conda activate $INSTALL_DIR
 
+# Basic installation tests
 srun -l -u python test_install.py --mpi --vision --geometric
+
+# Cray plugin training test
+exampleScript=/opt/cray/pe/craype-dl-plugin-py3/19.06.1/examples/torch_mnist/pytorch_mnist.py
+srun -u -l python $exampleScript --epochs 1 --log-interval 50
