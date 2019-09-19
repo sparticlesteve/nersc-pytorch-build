b86ef4f729272e28586d53cc41f51339b4df6517
Steve Farrell <sparticlesteve@gmail.com>
2019-09-19 14:11:20 -0700

    added requests package install to fix import error

--- build_geometric.sh
+++ build_geometric.sh
@@ -9,6 +9,7 @@ conda activate $INSTALL_DIR
 echo "Building PyTorch Geometric on $(hostname)"
 
 # Install the packages via pip
+pip install requests # fixes an import error
 pip install --verbose --no-cache-dir torch-scatter
 pip install --verbose --no-cache-dir torch-sparse
 pip install --verbose --no-cache-dir torch-cluster
