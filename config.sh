3372dc4d7c9b278078b0f69c23462e40defb307b
Steve Farrell <sparticlesteve@gmail.com>
2019-12-13 11:32:41 -0800

    gpu config

--- config.sh
+++ config.sh
@@ -13,16 +13,29 @@ export PYTHON_VERSION=3.6
 export PYTORCH_VERSION="v1.3.1"
 export PYTORCH_URL=https://github.com/pytorch/pytorch
 export VISION_VERSION="v0.4.2"
-export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION
-export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION
+export FOR_GPU=true
+if $FOR_GPU; then
+    export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION-gpu
+    export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION-gpu
+else
+    export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION
+    export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION
+fi
 
 # Setup programming environment
-module load gcc/8.2.0
-module unload PrgEnv-intel
-module load PrgEnv-gnu
-module unload craype-hugepages2M
-module unload cray-libsci
-module unload atp
+if $FOR_GPU; then
+    module purge
+    module load gcc/7.3.0
+    module load cuda/10.1.168
+    module load openmpi/4.0.1-ucx-1.6
+else
+    module load gcc/8.2.0
+    module unload PrgEnv-intel
+    module load PrgEnv-gnu
+    module unload craype-hugepages2M
+    module unload cray-libsci
+    module unload atp
+fi
 
 # Setup conda
 source /usr/common/software/python/3.7-anaconda-2019.07/etc/profile.d/conda.sh
