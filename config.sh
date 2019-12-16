d052bc462d9e22b0166a01088283e5454ab5155f
Steve Farrell <sparticlesteve@gmail.com>
2019-12-16 10:45:45 -0800

    update gpu config, use hardcoded arch flag

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-# Source me
+# Source me to setup config for the installations
 
 if [ $USER == "swowner" ]; then
     umask 002 # all-readable
@@ -13,8 +13,8 @@ export PYTHON_VERSION=3.6
 export PYTORCH_VERSION="v1.3.1"
 export PYTORCH_URL=https://github.com/pytorch/pytorch
 export VISION_VERSION="v0.4.2"
-export FOR_GPU=true
-if $FOR_GPU; then
+export SYSTEM_ARCH=gpu #cpu
+if [[ $SYSTEM_ARCH == "gpu" ]]; then
     export BUILD_DIR=$SCRATCH/pytorch-build/$INSTALL_NAME/$PYTORCH_VERSION-gpu
     export INSTALL_DIR=$INSTALL_BASE/$INSTALL_NAME/$PYTORCH_VERSION-gpu
 else
@@ -23,11 +23,24 @@ else
 fi
 
 # Setup programming environment
-if $FOR_GPU; then
+if [[ $SYSTEM_ARCH == "gpu" ]]; then
     module purge
+
+    # Vanilla setup
+    #module load gcc/7.3.0
+    #module load cuda/10.1.243
+    #module load mpich/3.3.1-debug
+
+    # OpenMPI setup
     module load gcc/7.3.0
     module load cuda/10.1.168
     module load openmpi/4.0.1-ucx-1.6
+
+    # Cutting edge mvapich
+    #module load gcc/8.2.0
+    #module load cuda/10.2.89
+    #module load mvapich2/2.3.2
+
 else
     module load gcc/8.2.0
     module unload PrgEnv-intel
@@ -41,6 +54,6 @@ fi
 source /usr/common/software/python/3.7-anaconda-2019.07/etc/profile.d/conda.sh
 
 # Print some stuff
-echo "Configuring on $(hostname) as $USER"
+echo "Configuring on $(hostname) as $USER for $SYSTEM_ARCH"
 echo "  Build directory $BUILD_DIR"
 echo "  Install directory $INSTALL_DIR"
