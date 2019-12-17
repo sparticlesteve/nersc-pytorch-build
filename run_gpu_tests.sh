7da0280f3e207e01731996760a2dcbd4281cd375
Steve Farrell <sparticlesteve@gmail.com>
2019-12-17 15:41:39 -0800

    update gpu tests script and adding several more distributed tests

--- run_gpu_tests.sh
+++ run_gpu_tests.sh
@@ -3,15 +3,47 @@
 . activate.sh
 module load esslurm
 
-# 1 node, 1 gpu
+echo "-------------------------------------------------------------------------"
+echo "Single GPU unit tests"
 srun -C gpu -N 1 --gres=gpu:8 -c 10 -t 5 -u python test_install.py --all
 
-# 1 node, 8 ranks
+echo "-------------------------------------------------------------------------"
+echo "Multi GPU unit tests"
 srun -C gpu -N 1 --gres=gpu:8 --exclusive \
-    -n 8 -c 10 -t 5 -u -l \
+    --ntasks-per-node 8 -c 10 -t 5 -u -l \
     python test_install.py --all
 
-# 2 nodes, 16 ranks
+echo "-------------------------------------------------------------------------"
+echo "Multi node, multi GPU unit tests"
 srun -C gpu -N 2 --gres=gpu:8 --exclusive \
-    -n 16 -c 10 -t 5 -u -l \
+    --ntasks-per-node 16 -c 10 -t 5 -u -l \
     python test_install.py --all
+
+echo "-------------------------------------------------------------------------"
+echo "DDP NCCL training test"
+srun -C gpu -N 1 --gres=gpu:8 --exclusive \
+    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+    python test_ddp.py --backend nccl-file
+
+echo "-------------------------------------------------------------------------"
+echo "DDP MPI training test"
+srun -C gpu -N 1 --gres=gpu:8 --exclusive \
+    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+    python test_ddp.py --backend mpi
+
+# Multi-node NCCL fails
+#srun -C gpu -N 2 --gres=gpu:8 --exclusive \
+#    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+#    python test_ddp.py --backend nccl-file
+
+echo "-------------------------------------------------------------------------"
+echo "DDP Gloo training test"
+srun -C gpu -N 1 --gres=gpu:8 --exclusive \
+    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+    python test_ddp.py --backend gloo-file
+
+echo "-------------------------------------------------------------------------"
+echo "DDP Gloo multi-node training test"
+srun -C gpu -N 2 --gres=gpu:8 --exclusive \
+    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+    python test_ddp.py --backend gloo-file
