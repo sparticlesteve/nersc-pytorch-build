acb99eecb27912e5583a4dc5cb9c378f32918872
Steve Farrell <sparticlesteve@gmail.com>
2019-12-20 13:45:39 -0800

    tests update

--- run_gpu_tests.sh
+++ run_gpu_tests.sh
@@ -10,19 +10,28 @@ echo "-------------------------------------------------------------------------"
 echo "Multi GPU unit tests"
 srun -C gpu -N 1 --gres=gpu:8 --exclusive \
     --ntasks-per-node 8 -c 10 -t 5 -u -l \
-    python test_install.py --all
+    python test_install.py --mpi --cuda
 
 echo "-------------------------------------------------------------------------"
 echo "Multi node, multi GPU unit tests"
 srun -C gpu -N 2 --gres=gpu:8 --exclusive \
     --ntasks-per-node 8 -c 10 -t 5 -u -l \
-    python test_install.py --all
+    python test_install.py --mpi --cuda
 
 echo "-------------------------------------------------------------------------"
 echo "DDP NCCL training test"
 srun -C gpu -N 1 --gres=gpu:8 --exclusive \
     --ntasks-per-node 8 -c 10 -t 5 -u -l \
     python test_ddp.py --backend nccl-file
+
+# Multi-node NCCL hangs
+#echo "-------------------------------------------------------------------------"
+#echo "DDP NCCL multi-node training test"
+#export NCCL_DEBUG=INFO
+#export NCCL_DEBUG_SUBSYS=ALL
+#srun -C gpu -N 2 --gres=gpu:8 --exclusive \
+#    --ntasks-per-node 8 -c 10 -t 5 -u -l \
+#    python test_ddp.py --backend nccl-file
 
 echo "-------------------------------------------------------------------------"
 echo "DDP MPI training test"
@@ -41,12 +50,3 @@ echo "DDP Gloo multi-node training test"
 srun -C gpu -N 2 --gres=gpu:8 --exclusive \
     --ntasks-per-node 8 -c 10 -t 5 -u -l \
     python test_ddp.py --backend gloo-file
-
-# Multi-node NCCL fails
-echo "-------------------------------------------------------------------------"
-echo "DDP NCCL multi-node training test"
-#export NCCL_DEBUG=INFO
-#export NCCL_DEBUG_SUBSYS=ALL
-srun -C gpu -N 2 --gres=gpu:8 --exclusive \
-    --ntasks-per-node 8 -c 10 -t 5 -u -l \
-    python test_ddp.py --backend nccl-file
