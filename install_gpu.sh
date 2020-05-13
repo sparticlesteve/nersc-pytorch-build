43a016e3c47a4e3725d3f7d22150217fa08d8e76
Steve Farrell <sparticlesteve@gmail.com>
2020-05-13 13:19:31 -0700

    update gpu bulid commands to use less resources

--- install_gpu.sh
+++ install_gpu.sh
@@ -14,9 +14,9 @@ module load esslurm
 ./checkout_packages.sh 2>&1 | tee log.checkout
 
 # Build pytorch on a GPU node
-srun -C gpu -N 1 --gres=gpu:8 --exclusive -t 4:00:00 \
+srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
     ./build_pytorch.sh 2>&1 | tee log.pytorch
 
 # Build pytorch geometric
-srun -C gpu -N 1 --gres=gpu:8 --exclusive -t 4:00:00 \
+srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
     ./build_geometric.sh 2>&1 | tee log.geometric
