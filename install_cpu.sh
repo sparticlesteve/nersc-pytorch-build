d22a7c194ac8b8f998c3dd9202bb9223be9406eb
Steve Farrell <sparticlesteve@gmail.com>
2020-07-29 17:09:24 -0700

    disabled cray plugin build

--- install_cpu.sh
+++ install_cpu.sh
@@ -7,4 +7,4 @@ set -e -o pipefail
 ./build_env.sh 2>&1 | tee log.env
 ./build_pytorch.sh 2>&1 | tee log.pytorch
 ./build_geometric.sh 2>&1 | tee log.geometric
-./build_craydl.sh 2>&1 | tee log.craydl
+#./build_craydl.sh 2>&1 | tee log.craydl
