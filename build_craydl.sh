acb35bb53bafe4d9acbbf3d0b4b440121e794461
Steve Farrell <sparticlesteve@gmail.com>
2020-07-29 22:34:00 -0700

    Update to latest cray dl plugin
    It doesn't build, however.

--- build_craydl.sh
+++ build_craydl.sh
@@ -5,14 +5,15 @@
 # We use the wheel installation method for builds with gcc >= 4.9 and c++11 ABI.
 #
 # See the documentation here:
-#   /opt/cray/pe/craype-dl-plugin-py3/19.06.1/wheel/README
+#   /opt/cray/pe/craype-dl-plugin-py3/20.06.1/wheel/README
 #
 
 # Setup our installation
 source activate.sh
 
 # Load the cray plugin module
-module load craype-dl-plugin-py3/19.06.1
+module load craype-dl-plugin-py3/20.06.1
 
-# The tarball wheel file there is broken; install this fixed one
-pip install /project/projectdirs/nstaff/swowner/craype-dl-plugin/dl_comm-19.6.1.tar.gz
+# Install the wheel
+pip install $CRAYPE_ML_PLUGIN_BASEDIR/wheel/dl_comm-20.6.1.tar.gz
+#pip install /project/projectdirs/nstaff/swowner/craype-dl-plugin/dl_comm-19.6.1.tar.gz
