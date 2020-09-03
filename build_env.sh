3f423644be46afb91e472799e04e4ac5ddd44420
Steve Farrell <sparticlesteve@gmail.com>
2020-09-03 10:25:32 -0700

    Adding ninja to conda installs

--- build_env.sh
+++ build_env.sh
@@ -5,7 +5,7 @@ source ./config.sh
 
 # Make a conda env
 conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION \
-    mkl mkl-include numpy pyyaml setuptools cmake cffi typing \
+    ninja mkl mkl-include numpy pyyaml setuptools cmake cffi typing \
     h5py ipython ipykernel matplotlib scikit-learn pandas pillow
 
 # Install additional packages
