b0a40cfcf1bc5cee83b42c6a4d1f9792d0826817
Steve Farrell <sparticlesteve@gmail.com>
2020-11-08 17:31:03 -0800

    disable mpi and geometric builds

--- install_gpu.sh
+++ install_gpu.sh
@@ -11,15 +11,18 @@ module load cgpu
 ./build_env.sh 2>&1 | tee log.env
 
 # Checkout software packages
-./checkout_packages.sh 2>&1 | tee log.checkout
+#./checkout_packages.sh 2>&1 | tee log.checkout
+
+# Install pytorch on a gpu node
+srun -C gpu -N 1 -G 1 -c 10 -t 10 ./install_pytorch.sh 2>&1 | tee log.pytorch
 
 # Build pytorch on a GPU node
-srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
-    ./build_pytorch.sh 2>&1 | tee log.pytorch
+#srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
+#    ./build_pytorch.sh 2>&1 | tee log.pytorch
 
 # Build pytorch geometric
-srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
-    ./build_geometric.sh 2>&1 | tee log.geometric
+#srun -C gpu -N 1 --gres=gpu:1 -c 40 -t 4:00:00 \
+#    ./build_geometric.sh 2>&1 | tee log.geometric
 
 # Build mpi4py
-./build_mpi4py.sh 2>&1 | tee log.mpi4py
+#./build_mpi4py.sh 2>&1 | tee log.mpi4py
