63db981f674f752d09aee442f4d9a6bb79176aab
Steve Farrell <sparticlesteve@gmail.com>
2021-01-04 22:22:17 -0800

    update ipympl version

--- build_env.sh
+++ build_env.sh
@@ -10,7 +10,7 @@ conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION \
 
 # Install additional packages
 conda activate $INSTALL_DIR
-conda install -y -c conda-forge ipympl=0.5.6
+conda install -y -c conda-forge ipympl=0.5.8
 pip install ray tensorboard tqdm wandb ruamel.yaml
 
 # Install NERSC tensorboard helper
