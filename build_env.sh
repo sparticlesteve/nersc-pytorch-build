2f4778342b39adf54c0743751ed630fcd3f6ec71
Steve Farrell <sparticlesteve@gmail.com>
2021-07-28 17:01:43 -0700

    switched to mamba install

--- build_env.sh
+++ build_env.sh
@@ -4,12 +4,12 @@
 source ./config.sh
 
 # Make a conda env
-conda create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION pip \
+mamba create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION pip \
     ninja mkl mkl-include numpy pyyaml setuptools cmake cffi typing \
     h5py ipython ipykernel matplotlib scikit-learn pandas pillow pytables
 
 # Install additional packages
 conda activate $INSTALL_DIR
-conda install -y -c conda-forge ipympl=0.7.0
+mamba install -y -c conda-forge ipympl=0.7.0
 pip install --no-cache-dir ray tensorboard tqdm wandb ruamel.yaml gpustat \
     git+https://github.com/NERSC/nersc-tensorboard-helper.git
