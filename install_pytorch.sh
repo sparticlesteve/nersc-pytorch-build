e3905c50a98f00a14dd0721ec952aa2b2608f014
Steve Farrell <sparticlesteve@gmail.com>
2021-07-29 16:01:52 -0700

    clean up some more commented code

--- install_pytorch.sh
+++ install_pytorch.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-# Setup the environment
-#source config.sh
-#conda activate $INSTALL_DIR
-
 # Install PyTorch, torchvision, and cuda via conda
 mamba install -y -c pytorch -c nvidia \
     pytorch=$PYTORCH_VERSION torchvision=$VISION_VERSION cudatoolkit=$CUDA_VERSION
