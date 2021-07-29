3f0879b568c0b035bd21df89818a44eb82dc924c
Steve Farrell <sparticlesteve@gmail.com>
2021-07-29 16:02:24 -0700

    moved ipympl to conda create command

--- build_env.sh
+++ build_env.sh
@@ -3,11 +3,11 @@
 # Make a conda env
 mamba create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION pip \
     ninja mkl mkl-include numpy pyyaml setuptools cmake cffi typing \
-    h5py ipython ipykernel matplotlib scikit-learn pandas pillow pytables
+    h5py ipython ipykernel matplotlib scikit-learn pandas pillow pytables \
+    ipympl=0.7.0
 
 # Install additional packages
 source $CONDA_INIT_SCRIPT
 conda activate $INSTALL_DIR
-mamba install -y -c conda-forge ipympl=0.7.0
 pip install --no-cache-dir ray tensorboard tqdm wandb ruamel.yaml gpustat \
     git+https://github.com/NERSC/nersc-tensorboard-helper.git
