3215e64dae8c6a6b09a0cc90d19e7258f15fd6ca
Steve Farrell <sparticlesteve@gmail.com>
2021-08-04 13:40:41 -0700

    moved tests to nersc-pytorch-testing repo

--- run_gpu_tests.sh
+++ run_gpu_tests.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -e
 #SBATCH -C gpu
 #SBATCH -N 1
-#SBATCH -G 8
-#SBATCH --exclusive
+#SBATCH --ntasks-per-node 8
+#SBATCH --gpus-per-node 8
 #SBATCH -c 10
 #SBATCH -t 30
 #SBATCH -o slurm-gpu-test-%j.out
 
+# Clone the testing repository
+[ -d nersc-pytorch-testing ] || git clone https://github.com/sparticlesteve/nersc-pytorch-testing.git
+
+# Setup software
 . config.sh
 conda activate $INSTALL_DIR
 
+# Run tests
+cd nersc-pytorch-testing
+
 echo "-------------------------------------------------------------------------"
 echo "Single GPU unit tests"
+srun -N 1 -n 1 -u python pytorch_info.py
 srun -N 1 -n 1 -u python test_install.py --cuda --vision --geometric
 
 #echo "-------------------------------------------------------------------------"
@@ -22,7 +30,8 @@ echo "-------------------------------------------------------------------------"
 echo "DDP NCCL training test"
 #export NCCL_DEBUG=INFO
 #export NCCL_DEBUG_SUBSYS=ALL
-srun --ntasks-per-node 8 -u -l python test_ddp.py --backend nccl-file --gpu
+srun --ntasks-per-node 8 -u -l python test_ddp.py --gpu --backend nccl --init-method slurm
+#srun --ntasks-per-node 8 -u -l python test_ddp.py --backend nccl-file --gpu
 
 # Disabling failing MPI test
 #echo "-------------------------------------------------------------------------"
@@ -31,7 +40,8 @@ srun --ntasks-per-node 8 -u -l python test_ddp.py --backend nccl-file --gpu
 
 echo "-------------------------------------------------------------------------"
 echo "DDP Gloo training test"
-srun --ntasks-per-node 8 -u -l python test_ddp.py --backend gloo-file --gpu
+srun --ntasks-per-node 8 -u -l python test_ddp.py --gpu --backend gloo --init-method slurm
+#srun --ntasks-per-node 8 -u -l python test_ddp.py --backend gloo-file --gpu
 
 echo "-------------------------------------------------------------------------"
 echo "PyTorch Geometric training test"
