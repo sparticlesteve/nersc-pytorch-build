f53cfc2aab3a83f6c88b951bc18623db09f49260
Steve Farrell <sparticlesteve@gmail.com>
2022-02-02 16:44:41 -0800

    remove commented config

--- config_cgpu.sh
+++ config_cgpu.sh
@@ -31,14 +31,6 @@ export LD_LIBRARY_PATH=$INSTALL_DIR/lib/python${PYTHON_VERSION}/site-packages/to
 export UCX_LOG_LEVEL=error
 export NCCL_IB_HCA=mlx5_0:1,mlx5_2:1,mlx5_4:1,mlx5_6:1
 
-# mvapich setup
-#module load gcc/7.3.0
-#module load cuda/10.1.168
-#module load mvapich2/2.3.2
-#export MV2_ENABLE_AFFINITY=0
-#export LD_PRELOAD=$MVAPICH2_DIR/lib/libmpi.so
-#export MV2_USE_CUDA=1
-
 # Setup conda
 export CONDA_INIT_SCRIPT=/usr/common/software/python/3.8-anaconda-2020.11/etc/profile.d/conda.sh
 source $CONDA_INIT_SCRIPT
