f2af0df832217c2991e28054e2da5f95a8fa2b90
Steve Farrell <sparticlesteve@gmail.com>
2024-05-21 10:08:57 -0700

    fix apex version to 22.03

--- build_apex.sh
+++ build_apex.sh
@@ -6,7 +6,8 @@
 echo "Building NVIDIA Apex"
 
 mkdir -p $BUILD_DIR && cd $BUILD_DIR
-git clone https://github.com/NVIDIA/apex
+[ -d apex ] && rm -rf apex
+git clone -b 22.03 https://github.com/NVIDIA/apex
 cd apex
 
 pip install -v --disable-pip-version-check --no-cache-dir \
