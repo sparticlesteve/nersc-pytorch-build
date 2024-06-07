a7eb10b23ba84a61171fd1872aa4f556b02b4bd0
Steve Farrell <sparticlesteve@gmail.com>
2024-06-07 14:51:21 -0700

    add huggingface packages

--- build_env.sh
+++ build_env.sh
@@ -9,10 +9,10 @@ mamba create -y --prefix $INSTALL_DIR python=$PYTHON_VERSION pip \
 # Install additional packages
 module load conda
 conda activate $INSTALL_DIR
-pip install --no-cache-dir ray ray[tune] tensorboard tqdm wandb ruamel.yaml gpustat \
-    pytest opencv-python scikit-image \
-    git+https://github.com/NERSC/nersc-tensorboard-helper.git \
-    torch_tb_profiler
+pip install --no-cache-dir ray ray[tune] tensorboard tqdm wandb ruamel.yaml \
+    accelerate datasets evaluate transformers \
+    gpustat pytest opencv-python scikit-image torch_tb_profiler \
+    git+https://github.com/NERSC/nersc-tensorboard-helper.git
 
 # Remove conda ld which causes problems
 rm $INSTALL_DIR/compiler_compat/ld
