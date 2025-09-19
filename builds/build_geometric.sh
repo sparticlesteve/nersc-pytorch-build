12525e1218706069fdc3e9cf8ce5af93013b0c78
Steve Farrell <sparticlesteve@gmail.com>
2025-09-19 16:10:40 -0700

    update pyg build

--- builds/build_geometric.sh
+++ builds/build_geometric.sh
@@ -1,49 +1,28 @@
 #!/bin/bash -e
 
 # This script installs PyTorch geometric and its dependencies
-# https://github.com/rusty1s/pytorch_geometric
+# https://github.com/pyg-team/pytorch_geometric
 
-echo "Building PyTorch Geometric"
+set -x
 
 # Drop the cray compiler wrappers, they aren't currently working here.
 export CXX=g++
 export CC=gcc
 export TORCH_CUDA_ARCH_LIST=8.0
+
 # Add lib paths to LIBRARY_PATH so linker can find them
-export LIBRARY_PATH=$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH}"
+# export LIBRARY_PATH=$LD_LIBRARY_PATH:$LIBRARY_PATH
+
 cd $BUILD_DIR
 
-# Build METIS from source
-# Mostly taken from here:
-# https://github.com/rusty1s/pytorch_sparse/blob/master/.github/workflows/metis.sh
-METIS=metis-5.1.0
-export WITH_METIS=1
-wget -nv "https://web.archive.org/web/20211119110155/http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/${METIS}.tar.gz"
-tar -xvzf ${METIS}.tar.gz
-cd ${METIS} || exit
-sed -i.bak -e 's/IDXTYPEWIDTH 32/IDXTYPEWIDTH 64/g' include/metis.h
-make config prefix=$INSTALL_DIR cc=gcc
-nice make -j
-make install
-
-# Build and install the packages via pip
+# Build and install the packages via pip, from source.
+# Note that PyG prebuilt wheels often fail against our glibc.
 export CPPFLAGS="-I${INSTALL_DIR}/include"
 export VERBOSE=1
-# pyg-lib currently not available in pypi, apparently
-#pip install --verbose --no-cache-dir pyg-lib
-pip install --verbose --no-cache-dir torch-scatter
-pip install --verbose --no-cache-dir torch-sparse
-pip install --verbose --no-cache-dir torch-cluster
-pip install --verbose torch-geometric
+pip install --verbose --no-cache-dir --no-build-isolation \
+    git+https://github.com/pyg-team/pyg-lib.git \
+    torch_scatter torch_sparse torch_cluster torch_geometric
 
-# Install wheels, often incompatible with source-built pytorch
-#pip install --verbose --no-cache-dir \
-#    pyg-lib torch-scatter torch-sparse torch-cluster torch-geometric \
-#    -f https://data.pyg.org/whl/torch-1.13.0+cu117.html
-
-# Install master versions directly from github (sometimes needed)
-#pip install --verbose --no-cache-dir git+https://github.com/pyg-team/pyg-lib.git
-#pip install --verbose --no-cache-dir git+https://github.com/rusty1s/pytorch_scatter.git
-#pip install --verbose --no-cache-dir git+https://github.com/rusty1s/pytorch_sparse.git
-#pip install --verbose --no-cache-dir git+https://github.com/rusty1s/pytorch_cluster.git
-#pip install --verbose --no-cache-dir git+https://github.com/rusty1s/pytorch_geometric.git
+# Quick sanity check that things are ok
+python -c "import torch, pyg_lib, torch_scatter, torch_sparse, torch_cluster, torch_geometric; print(torch.__version__, 'PyG OK')"
