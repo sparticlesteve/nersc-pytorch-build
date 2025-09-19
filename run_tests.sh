864a1be79f3a3f23420e34cc5ee37457c6804811
Steve Farrell <sparticlesteve@gmail.com>
2025-09-19 16:10:53 -0700

    update tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,18 @@
 #!/bin/bash -e
+#SBATCH -J pytorch-tests
 #SBATCH -C gpu
 #SBATCH -N 2
 #SBATCH --ntasks-per-node 4
 #SBATCH --gpus-per-node 4
 #SBATCH --cpus-per-task 32
 #SBATCH -t 30
-#SBATCH -o slurm-gpu-test-%j.out
+#SBATCH -o logs/slurm-gpu-test-%j.out
 
 # Clone the testing repository
 [ -d nersc-pytorch-testing ] || git clone https://github.com/sparticlesteve/nersc-pytorch-testing.git
 
 # Use local modulefiles
-module use /global/homes/s/sfarrell/WorkAreas/software/modulefiles/src
+#module use /global/homes/s/sfarrell/WorkAreas/software/modulefiles/src
 
 # Setup software
 source ./config/base_config.sh
